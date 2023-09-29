f7a89499d5f1ee2e7a1b9c42e107f3cb6dbf25ea
petrukhinandrew <dartmol2300@gmail.com>
2023-09-29 12:27:51 +0300

    added simple textfield

--- app/src/main/java/ru/spbu/mkn/schedule/MainActivity.kt
+++ app/src/main/java/ru/spbu/mkn/schedule/MainActivity.kt
@@ -3,11 +3,18 @@ package ru.spbu.mkn.schedule
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.spbu.mkn.schedule.ui.theme.MknScheduleAppTheme
@@ -29,12 +36,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var textFieldValue by remember { mutableStateOf<String>("") };
+    Column {
+        TextField(value = textFieldValue, onValueChange = { it -> textFieldValue = it })
+        Text(
+            text = "Hello $textFieldValue!",
+            modifier = modifier
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
