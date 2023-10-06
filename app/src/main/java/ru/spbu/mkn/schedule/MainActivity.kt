d6390a926cc35d936686ad17c4f4d90c00fb615a
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-10-06 11:03:48 +0300

    Group List

--- app/src/main/java/ru/spbu/mkn/schedule/MainActivity.kt
+++ app/src/main/java/ru/spbu/mkn/schedule/MainActivity.kt
@@ -3,8 +3,16 @@ package ru.spbu.mkn.schedule
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,11 +23,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.spbu.mkn.schedule.data.GroupsData
 import ru.spbu.mkn.schedule.ui.theme.MknScheduleAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GroupsList()
                 }
             }
         }
@@ -47,6 +62,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier
         )
 
+    }
+}
+
+
+@Composable
+fun GroupsList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
+        items(GroupsData.getGroupList()) { group ->
+            GroupItem(name = group, modifier = modifier)
+        }
+    }
+}
+
+
+@Composable
+fun GroupItem(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier.padding(8.dp),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            Text(
+                text = name, style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun PreviewGroupItemList() {
+    MknScheduleAppTheme {
+        GroupsList()
+    }
+}
+
+@Preview()
+@Composable
+fun PreviewGroupItem() {
+    MknScheduleAppTheme {
+        GroupItem(name = "Template")
     }
 }
 
