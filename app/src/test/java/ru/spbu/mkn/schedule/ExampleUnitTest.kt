8734ac756c80ded73983c935ecfeb067fdc152fa
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-10-06 12:19:26 +0300

    Optimize Imports

--- app/src/test/java/ru/spbu/mkn/schedule/ExampleUnitTest.kt
+++ app/src/test/java/ru/spbu/mkn/schedule/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ru.spbu.mkn.schedule
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
